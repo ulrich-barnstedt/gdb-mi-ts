e81fa5cdccf611adffbe4321eb60374db3983f37
ulrich-barnstedt <ulrich.barnstedt@gmail.com>
2024-05-17 02:27:22 +0200

    Add custom event emitter

---
+++ src/gdbEventEmitter.ts
@@ -0,0 +1,63 @@
+import {ActivityEvent, GDBEvent, ResultEvent, StatusEvent, StreamEvent} from "./gdbEvents.js";
+import {EK, Listener, TypedEventEmitter} from "./typedEventEmitter.js";
+
+export interface GDBEventMap extends Record<string | symbol, any[]> {
+    result: [ResultEvent],
+    status: [StatusEvent],
+    stream: [StreamEvent],
+    activity: [ActivityEvent],
+    all: [GDBEvent],
+    ready: []
+}
+
+export class GDBEventEmitter extends TypedEventEmitter<GDBEventMap> {
+    private finalListeners: Record<EK<GDBEventMap>, Listener<GDBEventMap, EK<GDBEventMap>>> = {};
+
+    public registerFinalListener <E extends EK<GDBEventMap>>(eventName: E, listener: Listener<GDBEventMap, E>) {
+        this.finalListeners[eventName] = listener;
+    }
+
+    private wrapListener <E extends EK<GDBEventMap>>(eventName: E, listener: Listener<GDBEventMap, E>): Listener<GDBEventMap, E> {
+        if (eventName === "ready") return listener;
+
+        return (...args) => {
+            if (args[0].isHandled) return;
+            listener(...args);
+        };
+    }
+
+    addListener<E extends EK<GDBEventMap>>(eventName: E, listener: Listener<GDBEventMap, E>): this {
+        listener = this.wrapListener(eventName, listener);
+        return super.addListener(eventName, listener);
+    }
+
+    on<E extends EK<GDBEventMap>>(eventName: E, listener: Listener<GDBEventMap, E>): this {
+        listener = this.wrapListener(eventName, listener);
+        return super.on(eventName, listener);
+    }
+
+    once<E extends EK<GDBEventMap>>(eventName: E, listener: Listener<GDBEventMap, E>): this {
+        listener = this.wrapListener(eventName, listener);
+        return super.once(eventName, listener);
+    }
+
+    prependListener<E extends EK<GDBEventMap>>(eventName: E, listener: Listener<GDBEventMap, E>): this {
+        listener = this.wrapListener(eventName, listener);
+        return super.prependListener(eventName, listener);
+    }
+
+    prependOnceListener<E extends EK<GDBEventMap>>(eventName: E, listener: Listener<GDBEventMap, E>): this {
+        listener = this.wrapListener(eventName, listener);
+        return super.prependOnceListener(eventName, listener);
+    }
+
+    emit <E extends EK<GDBEventMap>>(eventName: E, ...args: GDBEventMap[E]): boolean {
+        let res = super.emit(eventName, ...args);
+
+        if (eventName in this.finalListeners && !args[0].isHandled) {
+            this.finalListeners[eventName](args[0]);
+        }
+
+        return res;
+    }
+}
